90f84625bab7c3426707bd87f113d10b8f909a69
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2024-05-10 15:45:14 +0200

    Add lab05

---
+++ lab05/aggregation.lua
@@ -0,0 +1,79 @@
+S = 0.01
+W = 0.1
+P_s_max = 0.99
+P_w_min = 0.005
+alpha = 0.1
+beta = 0.05
+
+MAX_VELOCITY = 15
+MOVE_STEPS = 1000
+OBSTACLE_THRESHOLD = 0.1
+MAXRANGE = 30
+
+state = 0 -- 0 moving, 1 stopped
+--red stopped, green moving
+
+function init()
+    left_v = robot.random.uniform(0,MAX_VELOCITY)
+	right_v = robot.random.uniform(0,MAX_VELOCITY)
+	robot.wheels.set_velocity(left_v,right_v)
+    n_steps = 0
+    robot.leds.set_single_color(13, "green")
+end
+
+function random_stop(number_robot_sensed)
+    P_s = math.min(P_s_max, S+(alpha*number_robot_sensed))
+    t = robot.random.uniform()
+    if t <= P_s then
+        robot.wheels.set_velocity(0,0)
+        state = 1
+        robot.leds.set_single_color(13, "red")
+    end
+end
+
+function random_walk(number_robot_sensed)
+    P_w = math.max(P_w_min, W - (beta*number_robot_sensed))
+    t = robot.random.uniform()
+    if t <= P_w then
+        robot.wheels.set_velocity(robot.random.uniform(0, MAX_VELOCITY), robot.random.uniform(0, MAX_VELOCITY))
+        state = 0
+        robot.leds.set_single_color(13, "green")
+    end
+end
+
+function signal_presence(value)
+    robot.range_and_bearing.set_data(1,value)
+end
+
+function CountRAB()
+    number_robot_sensed = 0
+    for i = 1, #robot.range_and_bearing do
+        -- for each robot seen, check if it is close enough.
+        if robot.range_and_bearing[i].range < MAXRANGE and robot.range_and_bearing[i].data[1]==1 then
+            number_robot_sensed = number_robot_sensed + 1
+        end
+    end
+        return number_robot_sensed
+end    
+
+function step()
+    n_steps = n_steps + 1
+    number_robot_sensed = CountRAB()
+    random_stop(number_robot_sensed)
+    random_walk(number_robot_sensed)
+    signal_presence(state)
+    if n_steps > MOVE_STEPS then
+        reset()
+    end
+end
+
+function reset()
+    left_v = robot.random.uniform(0,MAX_VELOCITY)
+	right_v = robot.random.uniform(0,MAX_VELOCITY)
+	robot.wheels.set_velocity(left_v,right_v)
+	n_steps = 0
+end
+
+function destroy()
+    --
+  end
