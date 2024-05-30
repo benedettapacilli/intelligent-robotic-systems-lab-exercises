006dce0e5f9c03241db9d1da4fef58e091a2242d
benedettapacilli <benedetta.pacilli@studio.unibo.it>
2024-05-30 17:38:09 +0200

    Improve robot behavior

--- lab04/controller-motor_schemas.lua
+++ lab04/controller-motor_schemas.lua
@@ -1,5 +1,6 @@
-MAX_VELOCITY = 15
+MAX_VELOCITY = 10
 MOVE_STEPS = 200
+LIGHT_THRESHOLD = 0.55
 
 vector = require("vector")
 
@@ -12,45 +13,73 @@ function init()
 end
 
 function obstacle_avoidance()
+    if light_flag then
+        v = {length = 0, angle = 0}
+        return v
+    end
     proximity_sensors = robot.proximity
     max_value = -1
+    max_angle = -1
+    index = -1
     v = {length = 0, angle = 0}
     for i = 1, #proximity_sensors do
         if proximity_sensors[i].value > max_value then
             max_value = proximity_sensors[i].value
-            if i >= 1 or i <= 12 then
-                v = {length = proximity_sensors[i].value / max_value, angle = proximity_sensors[i].angle - math.pi}
-            else
-                v = {length = proximity_sensors[i].value / max_value, angle = proximity_sensors[i].angle + math.pi}
-            end
+            max_angle = proximity_sensors[i].angle
+            index = 1
+        end
+    end
+    if max_value ~= -1 then
+        if index <= #robot.proximity / 2 then
+            v = {length = MAX_VELOCITY, angle = max_angle + math.pi}
+        else
+            v = {length = MAX_VELOCITY, angle = max_angle - math.pi}
         end
     end
     return v
 end
 
-function phototaxis()
+function phototaxis(light_flag)
+    if light_flag then
+        v = {length = 0, angle = 0}
+        return v
+    end
     light_sensors = robot.light
     max_value = -1
+    max_angle = -1
     v = {length = 0, angle = 0}
     for i = 1, #light_sensors do
         if light_sensors[i].value > max_value then
             max_value = light_sensors[i].value
-            v = {length = ((light_sensors[i].value) * 10) / max_value, angle = light_sensors[i].angle}
+            max_angle = light_sensors[i].angle
         end
     end
+    if max_value ~= -1 then
+        v = {length = MAX_VELOCITY, angle = max_angle} -- max_value * 
+    end
     return v
+end
+
+function check_lighting()
+    for i = 1, #robot.light do
+        if robot.light[i].value >= LIGHT_THRESHOLD then
+            return true
+        end
+    end
+    return false
 end
 
 
 function step()
     n_steps = n_steps + 1
-    v_obstacle_avoidance = obstacle_avoidance()
-    v_phototaxis = phototaxis()
+    light_flag = check_lighting()
+    v_obstacle_avoidance = obstacle_avoidance(light_flag)
+    v_phototaxis = phototaxis(light_flag)
 
     v_final = vector.vec2_polar_sum(v_phototaxis, v_obstacle_avoidance)
 
-    left_v = 1 * v_phototaxis.length + (-L/2) * v_phototaxis.angle
-    right_v = 1 * v_phototaxis.length + (L/2) * v_phototaxis.angle
+    left_v = 1 * v_final.length + (-L/2) * v_final.angle
+    right_v = 1 * v_final.length + (L/2) * v_final.angle
     robot.wheels.set_velocity(left_v, right_v)
 
     if n_steps > MOVE_STEPS then
